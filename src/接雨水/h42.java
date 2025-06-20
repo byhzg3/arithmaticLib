d5083c2549337b126df95113805f95c3d751d332
byhzg3 <1257614202@qq.com>
2025-06-20 11:32:11 +0800

    k

---
+++ src/接雨水/h42.java
@@ -0,0 +1,20 @@
+package 接雨水;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ */
+public class h42 {
+    public int trap(int[] height) {
+        int result=0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i< height.length;i++){
+            if ()
+        }
+        if (stack.)
+        return result;
+    }
+}
